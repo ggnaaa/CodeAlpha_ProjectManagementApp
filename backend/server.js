ad4f18ec6d359db1658bb381e3f6c2c5d39a9696
Ramya B S <gaganashetty07@gmail.com>
2024-08-30 00:55:44 +0530

    project management app

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import api from './routes/index.js'
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config()
+mongoose.connect('mongodb+srv://gaganashetty07:hQR1wi2AY4Jm5kUW@cluster0.pdrn5dg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', () => {
+    console.log('connect');
+}, (e) => console.log(e))
+
+
+const PORT = process.env.SERVER_PORT || 9000
+const origin = process.env.CORS_ORIGIN || 'http://localhost:3000'
+
+const app = express()
+
+app.use(cors({
+    origin
+}));
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use(api)
+
+app.listen(PORT, () => {
+    console.log(`Your app is running in http://localhost:${PORT}`)
+})
